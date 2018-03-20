7390756ec81b4d2267fb39a14b0bfd9cb32a8358
Robin Bron <robin@finwo.nl>
2018-03-20 18:33:57 +0100

    Add test for array.intersect

---
+++ test/021-array-intersect.js
@@ -0,0 +1,27 @@
+var path   = require('path'),
+    assert = require('assert');
+
+// Force setting the polyfill
+process.env.TEST = 'Array.prototype.intersect';
+
+// Include the array fill helper
+require(path.join('..', 'lib', 'array_intersect'));
+
+var Mocha = global.Mocha || require('mocha');
+var Test  = Mocha.Test;
+var Suite = Mocha.Suite;
+var mocha = global.mocha || new Mocha();
+var suite = Suite.create(mocha.suite, 'Verifying Array.prototype.intersect');
+
+suite.timeout(5000);
+
+suite.addTest(new Test('Intersect on number entities', function () {
+  assert.equal(JSON.stringify([1, 2, 3].intersect([2, 3, 4])), '[2,3]');
+}));
+
+suite.addTest(new Test('Intersect on string entities', function () {
+  assert.equal(JSON.stringify(["foo", "bar", "hello"].intersect(["hello", "foo", "world"])), '["foo","hello"]');
+}));
+
+// Let's start testing
+mocha.run();
