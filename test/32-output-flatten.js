638ca8f983a60880470243f7de53c0a768a0fdc1
Robin Bron <robin@finwo.nl>
2018-03-26 13:41:58 +0200

    Included functions into the flatten as well

--- test/32-output-flatten.js
+++ test/32-output-flatten.js
@@ -11,7 +11,7 @@ var Mocha = global.Mocha || require('mocha');
 var Test  = Mocha.Test;
 var Suite = Mocha.Suite;
 var mocha = global.mocha || new Mocha();
-var suite = Suite.create(mocha.suite, 'Verifying module.exports.set');
+var suite = Suite.create(mocha.suite, 'Verifying module.exports.flatten');
 
 suite.timeout(5000);
 
@@ -29,6 +29,11 @@ suite.addTest(new Test('Flatten a deep object', function () {
 
 suite.addTest(new Test('Flatten a deep object, including json of objects', function () {
   assert.equal(JSON.stringify(flatten({header:{from:"some1@example.com"}}, true)), '{"header":"{\\"from\\":\\"some1@example.com\\"}","header.from":"some1@example.com"}');
+}));
+
+suite.addTest(new Test('Flatten a deep object containing a function', function () {
+  assert.equal(JSON.stringify(flatten({header: console.log})), '{}');
+  assert.equal(typeof flatten({header: { log: console.log }})['header.log'], 'function');
 }));
 
 // suite.addTest(new Test('Setting a first-level string by string key', function () {
