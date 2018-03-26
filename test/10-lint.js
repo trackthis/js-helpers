58f68b5a7faf15ecee4af6b0414e5c431909b447
Robin Bron <robin@finwo.nl>
2018-03-26 13:12:06 +0200

    Scrutinizer remark

--- test/10-lint.js
+++ test/10-lint.js
@@ -31,12 +31,12 @@ suite.addTest(new Test('Verifying file list',function() {
 co(function* () {
   files = (yield fs.scandir(approot))
     .filter(function (filename) {
-      if ( filename.substr(-3) !== '.js' ) return false;
-      if ( filename.substr(-7) === '.min.js' ) return false;
-      if ( filename.indexOf(path.sep + '.git' + path.sep) >= 0 ) return false;
-      if ( filename.indexOf(path.sep + '.idea' + path.sep) >= 0 ) return false;
-      if ( filename.indexOf(path.sep + 'coverage' + path.sep) >= 0 ) return false;
-      if ( filename.indexOf(path.sep + 'node_modules' + path.sep) >= 0 ) return false;
+      if ( filename.substr(-3) !== '.js' ) { return false; }
+      if ( filename.substr(-7) === '.min.js' ) { return false; }
+      if ( filename.indexOf(path.sep + '.git' + path.sep) >= 0 ) { return false; }
+      if ( filename.indexOf(path.sep + '.idea' + path.sep) >= 0 ) { return false; }
+      if ( filename.indexOf(path.sep + 'coverage' + path.sep) >= 0 ) { return false; }
+      if ( filename.indexOf(path.sep + 'node_modules' + path.sep) >= 0 ) { return false; }
       return true;
     });
 
