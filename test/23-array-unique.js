9369be185d89fa9432f0c39b1d1e7c472b4c74a7
Robin Bron <robin@finwo.nl>
2018-03-26 13:15:55 +0200

    Added test for Array.prototype.unique

---
+++ test/23-array-unique.js
@@ -0,0 +1,31 @@
+var path   = require('path'),
+    assert = require('assert');
+
+// Force setting the polyfill
+process.env.TEST = 'Array.prototype.intersect';
+
+// Include the array intersect helper
+require(path.join('..', 'lib', 'array_unique'));
+
+var Mocha = global.Mocha || require('mocha');
+var Test  = Mocha.Test;
+var Suite = Mocha.Suite;
+var mocha = global.mocha || new Mocha();
+var suite = Suite.create(mocha.suite, 'Verifying Array.prototype.unique');
+
+suite.timeout(5000);
+
+suite.addTest(new Test('Already-unique array', function () {
+  assert.equal(JSON.stringify([1, 2, 3].unique()), '[1,2,3]');
+}));
+
+suite.addTest(new Test('All duplicate array', function () {
+  assert.equal(JSON.stringify([ 1, 1, 1 ].unique()), '[1]');
+}));
+
+suite.addTest(new Test('Single duplicate value', function () {
+  assert.equal(JSON.stringify([ 3, 2, 2, 1 ].unique()), '[3,2,1]');
+}));
+
+// Let's start testing
+mocha.run();
