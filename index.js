cff50528e7d958f3d0a7e3d1143f9dfdbdfffd64
Robin Bron <robin@finwo.nl>
2018-03-27 14:07:38 +0200

    Added Eli Grey's object.watch polyfill

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('./lib/array_intersect');
 require('./lib/array_qsort');
 require('./lib/array_unique');
 require('./lib/fs_scandir');
+require('./lib/object_watch');
 require('./lib/string_format');
 require('./lib/string_pipe');
 
