4cbb6f9c41485e7956c786d89a6062e5bb7cc392
Robin Bron <robin@finwo.nl>
2018-10-03 11:18:01 +0200

    Working on a clean version

--- index.js
+++ index.js
@@ -1,43 +1,48 @@
-module.exports = function( options ) {
-  options = ( 'object' === typeof options ) ? ( options || {} ) : {};
+module.exports = function( { fs=false, hygienic=false, fix={} } = {} ) {
 
-  options.fs             = ('object'===typeof window) ? false : (options.fs || false);
-  options.hygienic       = options.hygienic       || false;
-  options.fix            = options.fix            || {};
-  options.fix.browserify = options.fix.browserify || false;
-  options.fix.mocha      = options.fix.mocha      || false;
-
-  // String prototype extension
-  if(!options.hygienic) {
-    require('./lib/string_format');
-    require('./lib/string_hashcode');
-    if(!options.fix.browserify) {
-      require('./lib/string_pipe');
-    }
+  if (!hygienic) {
+    require('./lib/array-fill')
   }
 
-  // Array prototype extension
-  if(!options.hygienic) {
-    require('./lib/array_fill');
-    require('./lib/array_intersect');
-    require('./lib/array_qsort');
-    require('./lib/array_unique');
-  }
-
-  // Object prototype extension
-  if(!options.hygienic) {
-    require('./lib/object_watch')( options.fix.mocha ||
-      ((('undefined' !== typeof process) && process.env && ('string' === typeof process.env.DEBUG) && (!isNaN(process.env.DEBUG))) ? !!parseInt(process.env.DEBUG) : false));
-  }
-
-  // Add fs.scandir
-  if(options.fs) {
-    require('./lib/fs_scandir')(options.fs);
-  }
-
-  return {
-    flatten: require('./lib/object_flatten'),
-    get    : require('./lib/get_deep'),
-    set    : require('./lib/set_deep'),
-  };
+  // options = ( 'object' === typeof options ) ? ( options || {} ) : {};
+  //
+  // options.fs             = ('object'===typeof window) ? false : (options.fs || false);
+  // options.hygienic       = options.hygienic       || false;
+  // options.fix            = options.fix            || {};
+  // options.fix.browserify = options.fix.browserify || false;
+  // options.fix.mocha      = options.fix.mocha      || false;
+  //
+  // // String prototype extension
+  // if(!options.hygienic) {
+  //   require('./lib/string_format');
+  //   require('./lib/string_hashcode');
+  //   if(!options.fix.browserify) {
+  //     require('./lib/string_pipe');
+  //   }
+  // }
+  //
+  // // Array prototype extension
+  // if(!options.hygienic) {
+  //   require('./lib/array_fill');
+  //   require('./lib/array_intersect');
+  //   require('./lib/array_qsort');
+  //   require('./lib/array_unique');
+  // }
+  //
+  // // Object prototype extension
+  // if(!options.hygienic) {
+  //   require('./lib/object_watch')( options.fix.mocha ||
+  //     ((('undefined' !== typeof process) && process.env && ('string' === typeof process.env.DEBUG) && (!isNaN(process.env.DEBUG))) ? !!parseInt(process.env.DEBUG) : false));
+  // }
+  //
+  // // Add fs.scandir
+  // if(options.fs) {
+  //   require('./lib/fs_scandir')(options.fs);
+  // }
+  //
+  // return {
+  //   flatten: require('./lib/object_flatten'),
+  //   get    : require('./lib/get_deep'),
+  //   set    : require('./lib/set_deep'),
+  // };
 };
