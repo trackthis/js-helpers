4cd68e70c3a7171d1b32d638ec2875258a8b43d3
Robin Bron <robin@finwo.nl>
2018-10-03 13:01:29 +0200

    Removed useless comment

--- index.js
+++ index.js
@@ -52,39 +52,4 @@ module.exports = function (options) {
     get: require('./get'),
     set: require('./set'),
   }
-
-
-  // // String prototype extension
-  // if(!options.hygienic) {
-  //   require('./lib/string_format');
-  //   require('./lib/string_hashcode');
-  //   if(!options.fix.browserify) {
-  //     require('./lib/string_pipe');
-  //   }
-  // }
-  //
-  // // Array prototype extension
-  // if(!options.hygienic) {
-  //   require('./lib/array_fill');
-  //   require('./lib/array_intersect');
-  //   require('./lib/array_qsort');
-  //   require('./lib/array_unique');
-  // }
-  //
-  // // Object prototype extension
-  // if(!options.hygienic) {
-  //   require('./lib/object_watch')( options.fix.mocha ||
-  //     ((('undefined' !== typeof process) && process.env && ('string' === typeof process.env.DEBUG) && (!isNaN(process.env.DEBUG))) ? !!parseInt(process.env.DEBUG) : false));
-  // }
-  //
-  // // Add fs.scandir
-  // if(options.fs) {
-  //   require('./lib/fs_scandir')(options.fs);
-  // }
-  //
-  // return {
-  //   flatten: require('./lib/object_flatten'),
-  //   get    : require('./lib/get_deep'),
-  //   set    : require('./lib/set_deep'),
-  // };
 };
